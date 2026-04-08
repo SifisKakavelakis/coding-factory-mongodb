f9e2352cf7abc0e51388b8f5e24d9fc89fe6290c
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-04-08 15:35:40 +0300

    feat: Express middleware and POST requests with JSON body - express.json() middleware for parsing request body - Custom logger middleware - GET /user - POST /user with logger middleware and req.body

---
+++ myapp2/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const logger = (req, res, next) => {
+    let url = req.url;
+    console.log("Logger", url);
+    let time = new Date();
+    console.log('Received request for ' + url + ' at ' + time);
+    next();
+
+}
+
+app.get('/user', (req, res) => {
+    res.send("User app");
+})
+
+app.post('/user', logger, (req, res) => {
+    console.log(req.body);
+    let user = req.body;
+    let name = user.name;
+    let surname = user.surname;
+
+    console.log("Post variables", name, surname);
+    res.send('Name ' + name + ' Surname' + surname);
+})
+
+app.listen(3000, ()=> {
+    console.log("Server is up");
+})
